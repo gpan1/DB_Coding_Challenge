7c6c56e34bbc6d8ed7a19a361f503fc5a938fec9
Spaghetticat1 <jackelincastrov@gmail.com>
2023-08-03 16:33:10 -0400

    Update csvtosql.py
    adding to python script

--- java-api/src/main/resources/other/csvtosql.py
+++ java-api/src/main/resources/other/csvtosql.py
@@ -4,7 +4,24 @@ import csv
 csv_file_path = 'C:\\Users\\jacki\\DB_Coding_Challenge\\java-api\\src\\main\\resources\\other\\db-bonds-data.csv'
 csv_file = open(csv_file_path, 'r')
 
-# Define the table name and column names
+# # Populates Book table
+# table_name = 'book'
+# columns = ['book_name',]
+
+# # Iterate over each row in the CSV file
+# csv_reader = csv.DictReader(csv_file)
+
+# book_list = [] need to finish creating list
+
+# for row in csv_reader:
+#     # Build the SQL INSERT statement
+#     values = ', '.join([f"'{row[column]}'" for column in columns])
+#     sql = f"INSERT INTO {table_name} (name) VALUES ({values});"
+
+#     # Print the SQL statement
+#     print(sql)
+
+# Populates Book table
 table_name = 'book'
 columns = ['book_name',]
 
@@ -16,7 +33,7 @@ for row in csv_reader:
     sql = f"INSERT INTO {table_name} (name) VALUES ({values});"
 
     # Print the SQL statement
-    print(sql)
+    print(sql)    
 
 # Close the CSV file
 csv_file.close()
