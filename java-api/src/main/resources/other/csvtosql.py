bc27727b6fb47bf02a678fbab36e77c3cd189a50
Spaghetticat1 <jackelincastrov@gmail.com>
2023-08-04 20:55:39 -0400

    added inserts to everything except trades and book_user table

--- java-api/src/main/resources/other/csvtosql.py
+++ java-api/src/main/resources/other/csvtosql.py
@@ -1,41 +1,20 @@
 import csv
 
+
 # Open the CSV file
 csv_file_path = 'C:\\Users\\jacki\\DB_Coding_Challenge\\java-api\\src\\main\\resources\\other\\db-bonds-data.csv'
 csv_file = open(csv_file_path, 'r')
-
-# # Populates Book table
-# table_name = 'book'
-# columns = ['book_name',]
-
-# # Iterate over each row in the CSV file
-# csv_reader = csv.DictReader(csv_file)
-
-# book_list = [] need to finish creating list
-
-# for row in csv_reader:
-#     # Build the SQL INSERT statement
-#     values = ', '.join([f"'{row[column]}'" for column in columns])
-#     sql = f"INSERT INTO {table_name} (name) VALUES ({values});"
-
-#     # Print the SQL statement
-#     print(sql)
-
-# Populates Book table
-table_name = 'book'
-columns = ['book_name',]
-
-# Iterate over each row in the CSV file
 csv_reader = csv.DictReader(csv_file)
+
+
+count = 1
+
 for row in csv_reader:
-    # Build the SQL INSERT statement
-    values = ', '.join([f"'{row[column]}'" for column in columns])
-    sql = f"INSERT INTO {table_name} (name) VALUES ({values});"
+    sql = "insert into security (id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status)"
+    sql += f" values ({count},'{row['isin']}','{row['cusip']}','{row['issuer_name']}','{row['bond_maturity_date']}',{row['coupon_percent']},'{row['type']}',{row['face_value (mn)']},'{row['trade_currency']}','{row['trade_status']});"
+    print(sql)
+    count+=1
 
-    # Print the SQL statement
-    print(sql)    
-
-# Close the CSV file
 csv_file.close()
 
 
