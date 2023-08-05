c7b016750d1af6a470bde12c320942e2d5517050
Spaghetticat1 <jackelincastrov@gmail.com>
2023-08-04 21:47:00 -0400

    ready to test with small amount of data

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -11,6 +11,45 @@ insert into book(id, name) values (4, 'Trading_book_4');
 insert into book(id, name) values (5, 'Trading_book_5');
 insert into book(id, name) values (6, 'Trading_book_6');
 
+--USERS table insertions
+
+insert into USERS  (ID ,NAME,EMAIL,ROLE)
+values (1, 'AZ Holdings Inc', 'AZ@email.com','role');
+insert into USERS  (ID ,NAME,EMAIL,ROLE)
+values (2, 'AcmeCo', 'AcmeCo@email.com','role');
+
+--COUNTERPARTY table insertions
+
+insert into COUNTERPARTY (ID ,NAME)
+values (1, 'AZ Holdings Inc');
+insert into COUNTERPARTY (ID ,NAME)
+values (2, 'Acme co');
+
+
+--SECURITY table real insertions
+
+insert into security (id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status) 
+values (1,'XS1988387210', 'BNPParibasIssu', '4.37 Microsoft Corp (USD)','5/8/2021',4.37,'CORP',1000,'USD','open');
+insert into security (id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status) 
+values (2, 'XS1988387210', 'BNPParibasIssu', '4.37 Microsoft Corp (USD)', '5/8/2021', 4.37, 'CORP', 1000, 'GBP', 'open');
+
+--insert into trades table
+
+INSERT INTO trades (book_id, security_id, counterparty_id, currency, status, quantity, unit_price, buy_sell, trade_date, settlement_date) VALUES
+  (1, 'XS1988387210', 1, 'USD', 'open', 50, 90, 'buy', '2021/05/13', '2021/08/04'),
+  (2, 'XS1988387210', 2, 'GBP', 'open', 40, 89.56, 'sell', '2021/02/04', '2021/08/04');
+
+--insert into book_user table
+
+INSERT INTO book_user (book_id, users_id) 
+VALUES (1, 1),(2, 2);
+
+
+
+
+
+
+
 ---security table fake insertions
 
 -- insert into security(id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status) 
@@ -22,28 +61,8 @@ insert into book(id, name) values (6, 'Trading_book_6');
 -- insert into security(id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status)
 -- values (4, 'ISIN_4', 'CUSIP_4', 'Issuer_4', '2020-01-04', 4.0, 'Type_4', 4.0, 'USD', 'Status_4');
 
---SECURITY table real insertions
-
-insert into security (id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status) 
-values (1,'XS1988387210', 'BNPParibasIssu', '4.37 Microsoft Corp (USD)','5/8/2021',4.37,'CORP',1000,'USD','open');
-insert into security (id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status) 
-values (2, 'XS1988387210', 'BNPParibasIssu', '4.37 Microsoft Corp (USD)', '5/8/2021', 4.37, 'CORP', 1000, 'GBP', 'open');
 
 
---COUNTERPARTY table insertions
-
-insert into COUNTERPARTY (ID ,NAME)
-values (1, 'AZ Holdings Inc')
-insert into COUNTERPARTY (ID ,NAME)
-values (2, 'Acme co')
-
---USERS table insertions
-
-insert into USERS  (ID ,NAME,EMAIL,ROLE)
-values (1, 'AZ Holdings Inc', 'AZ@email.com','role')
-insert into USERS  (ID ,NAME,EMAIL,ROLE)
-values (2, 'AcmeCo', 'AcmeCo@email.com','role')
 
 
--- insert into TRADES (ID , BOOK_ID, SECURITY_ID, COUNTERPARTY_ID, CURRENCY , STATUS, QUANTITY , UNIT_PRICE, BUY_SELL ,TRADE_DATE ,SETTLEMENT_DATE )
--- values (1, 1, 'XS1988387210', 1, 'USD', 'open', 50,90,'buy','13/05/2021','4/8/2021')
+
